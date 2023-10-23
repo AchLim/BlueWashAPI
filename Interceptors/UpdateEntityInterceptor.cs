6ef03fa8e6f518d484042123e9e746486adc2c48
AchLim <rimuakoru@gmail.com>
2023-10-24 00:50:19 +0700

    [COMMENT] UpdateEntityInterceptor

--- Interceptors/UpdateEntityInterceptor.cs
+++ Interceptors/UpdateEntityInterceptor.cs
@@ -7,32 +7,32 @@ namespace WebAPI.Interceptors
 {
     public class UpdateEntityInterceptor : SaveChangesInterceptor
     {
-        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
-        {
-            DbContext? dbContext = eventData.Context;
+        //public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        //{
+        //    DbContext? dbContext = eventData.Context;
 
-            if (dbContext is null)
-            {
-                return base.SavingChangesAsync(eventData, result, cancellationToken);
-            }
+        //    if (dbContext is null)
+        //    {
+        //        return base.SavingChangesAsync(eventData, result, cancellationToken);
+        //    }
 
-            IEnumerable<EntityEntry<AuditableEntity>> auditableEntities = dbContext
-                                                                            .ChangeTracker
-                                                                            .Entries<AuditableEntity>();
+        //    IEnumerable<EntityEntry<AuditableEntity>> auditableEntities = dbContext
+        //                                                                    .ChangeTracker
+        //                                                                    .Entries<AuditableEntity>();
 
-            foreach (var entity in auditableEntities)
-            {
-                if (entity.State == EntityState.Added)
-                {
-                    entity.Property(a => a.Created).CurrentValue = DateTime.UtcNow;
-                }
-                if (entity.State == EntityState.Modified)
-                {
-                    entity.Property(a => a.LastModified).CurrentValue = DateTime.UtcNow;
-                }
-            }
+        //    foreach (var entity in auditableEntities)
+        //    {
+        //        if (entity.State == EntityState.Added)
+        //        {
+        //            entity.Property(a => a.Created).CurrentValue = DateTime.UtcNow;
+        //        }
+        //        if (entity.State == EntityState.Modified)
+        //        {
+        //            entity.Property(a => a.LastModified).CurrentValue = DateTime.UtcNow;
+        //        }
+        //    }
 
-            return base.SavingChangesAsync(eventData, result, cancellationToken);
-        }
+        //    return base.SavingChangesAsync(eventData, result, cancellationToken);
+        //}
     }
 }
