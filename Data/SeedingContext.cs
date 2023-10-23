881b49a67331620e99e4aefa2cc18b56a0961df6
AchLim <rimuakoru@gmail.com>
2023-10-24 00:49:56 +0700

    [ADD] Seeds for Chart of Account, User & Role [UPDATE] Only seed 4 currencies now.

---
+++ Data/SeedingContext.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using WebAPI.DAL;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace Book_Lending_System.Data
+{
+    public static class ContextSeed
+    {
+        //Seed Roles
+        public static readonly string Admin = "ADMIN";
+        public static readonly string Manager = "MANAGER";
+        public static readonly string Staff = "STAFF";
+
+        public static async Task SeedRolesAsync(ApplicationContext context)
+        {
+            List<ApplicationRole> roles = await context.Roles.AsNoTracking().ToListAsync();
+
+            await using (var transaction = await context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    if (roles.Find(r => r.Name == Admin) is null)
+                        await context.Roles.AddAsync(new ApplicationRole { Name = Admin });
+
+                    if (roles.Find(r => r.Name == Staff) is null)
+                        await context.Roles.AddAsync(new ApplicationRole { Name = Staff });
+
+                    if (roles.Find(r => r.Name == Manager) is null)
+                        await context.Roles.AddAsync(new ApplicationRole { Name = Manager });
+
+                    await context.SaveChangesAsync();
+                    await transaction.CommitAsync();
+                }
+                catch
+                {
+                    await transaction.RollbackAsync();
+                    throw;
+                }
+            }
+        }
+
+        public static async Task SeedUsersAsync(ApplicationContext context)
+        {
+            await using (var transaction = await context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    // Admin
+                    if (await context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Username.ToUpper() == Admin) is null)
+                    {
+                        var adminUser = new ApplicationUser
+                        {
+                            Username = "admin",
+                            Login = Admin,
+                            EmailAddress = "bluewash.admin@example.com",
+                            PasswordHash = BCrypt.Net.BCrypt.HashPassword("b1u3w45h_4dm1n"),
+                        };
+
+                        await context.Users.AddAsync(adminUser);
+                        await context.SaveChangesAsync();
+
+                        List<ApplicationRole> roles = await context.Roles.AsNoTracking().ToListAsync();
+                        List<ApplicationUserRole> userRoles = new(roles.Count);
+                        foreach (var role in roles)
+                        {
+                            userRoles.Add(new ApplicationUserRole
+                            {
+                                ApplicationUserId = adminUser.Id,
+                                ApplicationRoleId = role.Id
+                            });
+                        }
+
+                        await context.UserRoles.AddRangeAsync(userRoles);
+                        await context.SaveChangesAsync();
+                    }
+
+                    // Manager
+                    if (await context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Username.ToUpper() == Manager) is null)
+                    {
+                        var managerUser = new ApplicationUser
+                        {
+                            Username = "manager",
+                            Login = Manager,
+                            EmailAddress = "bluewash.manager@example.com",
+                            PasswordHash = BCrypt.Net.BCrypt.HashPassword("b1u3w45h_m4n493r"),
+                        };
+
+                        await context.Users.AddAsync(managerUser);
+                        await context.SaveChangesAsync();
+
+                        ApplicationRole? managerRole = await context.Roles.AsNoTracking().FirstOrDefaultAsync(u => u.Name.ToUpper() == Manager);
+
+                        if (managerRole is not null)
+                        {
+                            await context.UserRoles.AddAsync(new ApplicationUserRole
+                            {
+                                ApplicationUserId = managerUser.Id,
+                                ApplicationRoleId = managerRole.Id
+                            });
+                            await context.SaveChangesAsync();
+                        }
+                    }
+
+                    // Staff
+                    if (await context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Username.ToUpper() == Staff) is null)
+                    {
+                        var staffUser = new ApplicationUser
+                        {
+                            Username = "staff",
+                            Login = Staff,
+                            EmailAddress = "bluewash.user@example.com",
+                            PasswordHash = BCrypt.Net.BCrypt.HashPassword("b1u3w45h_5t4ff"),
+                        };
+
+                        await context.Users.AddAsync(staffUser);
+                        await context.SaveChangesAsync();
+
+                        ApplicationRole? staffRole = await context.Roles.AsNoTracking().FirstOrDefaultAsync(u => u.Name.ToUpper() == Staff);
+
+                        if (staffRole is not null)
+                        {
+                            await context.UserRoles.AddAsync(new ApplicationUserRole
+                            {
+                                ApplicationUserId = staffUser.Id,
+                                ApplicationRoleId = staffRole.Id
+                            });
+                            await context.SaveChangesAsync();
+                        }
+                    }
+
+
+
+                    await transaction.CommitAsync();
+                }
+                catch
+                {
+                    await transaction.RollbackAsync();
+                    throw;
+                }
+            }
+        }
+    }
+}
--- Utility/ModelBuilderExtension.cs
+++ Utility/ModelBuilderExtension.cs
@@ -2,38 +2,67 @@
 using WebAPI.Models;
 using System.Globalization;
 using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Identity;
 
 namespace WebAPI.Utility
 {
     public static class ModelBuilderExtension
     {
-        public static void Seed(this ModelBuilder modelBuilder)
+        public static void SeedCurrency(this ModelBuilder modelBuilder)
         {
-            Span<CultureInfo> cultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures).AsSpan();
-            List<Currency> Currencies = new(cultures.Length);
+            //Span<CultureInfo> cultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures).AsSpan();
+            //List<Currency> Currencies = new(cultures.Length);
 
-            foreach (var culture in cultures)
+            //foreach (var culture in cultures)
+            //{
+            //    if (culture.LCID == 0x1000)
+            //        continue;
+
+            //    RegionInfo region = new(culture.LCID);
+
+            //    var name = region.EnglishName;
+            //    var code = region.ISOCurrencySymbol;
+            //    var cultureName = culture.Name;
+
+            //    Currencies.Add(new Currency
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = name,
+            //        Code = code,
+            //        CultureName = cultureName
+            //    });
+            //}
+
+            List<Currency> Currencies = new(4)
             {
-                if (culture.LCID == 0x1000)
-                    continue;
-
-                RegionInfo region = new(culture.LCID);
-
-                var name = region.EnglishName;
-                var code = region.ISOCurrencySymbol;
-                var cultureName = culture.Name;
-
-                Currencies.Add(new Currency
-                {
-                    Id = Guid.NewGuid(),
-                    Name = name,
-                    Code = code,
-                    CultureName = cultureName
-                });
-
-            }
-
+                new Currency { Id = Guid.NewGuid(), Name = "Indonesia Rupiah", Code = "IDR", CultureName = "id-ID" },
+                new Currency { Id = Guid.NewGuid(), Name = "Dollar Singapore", Code = "SGD", CultureName = "en-SG" },
+                new Currency { Id = Guid.NewGuid(), Name = "Ringgit Malaysia", Code = "MYR", CultureName = "ms-MY" },
+                new Currency { Id = Guid.NewGuid(), Name = "Dollar USD", Code = "USD", CultureName = "en-US" },
+            };
             modelBuilder.Entity<Currency>().HasData(Currencies);
+        }
+        public static void SeedChartOfAccount(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ChartOfAccount>().HasData(new List<ChartOfAccount>(15)
+            {
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 100, AccountHeaderName = "Asset", AccountNo = 111, AccountName = "Kas" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 100, AccountHeaderName = "Asset", AccountNo = 112, AccountName = "Bank" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 100, AccountHeaderName = "Asset", AccountNo = 113, AccountName = "Persediaan" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 100, AccountHeaderName = "Asset", AccountNo = 114, AccountName = "Perlengkapan" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 100, AccountHeaderName = "Asset", AccountNo = 115, AccountName = "Sewa dibayar di muka" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 100, AccountHeaderName = "Asset", AccountNo = 121, AccountName = "Peralatan" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 100, AccountHeaderName = "Asset", AccountNo = 122, AccountName = "Akumulasi Depresiasi - Mesin Cuci" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 200, AccountHeaderName = "Liabilitas", AccountNo = 201, AccountName = "Utang Usaha" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 300, AccountHeaderName = "Ekuitas", AccountNo = 301, AccountName = "Ekuitas Pemilik Usaha" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 400, AccountHeaderName = "Pendapatan", AccountNo = 401, AccountName = "Pendapatan Penjualan" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 500, AccountHeaderName = "Pengeluaran", AccountNo = 501, AccountName = "Beban Gaji" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 500, AccountHeaderName = "Pengeluaran", AccountNo = 502, AccountName = "Beban Sewa" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 500, AccountHeaderName = "Pengeluaran", AccountNo = 503, AccountName = "Beban Utilitas" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 500, AccountHeaderName = "Pengeluaran", AccountNo = 504, AccountName = "Beban Listrik" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 500, AccountHeaderName = "Pengeluaran", AccountNo = 505, AccountName = "Beban Perlengkapan" },
+                new ChartOfAccount { Id = Guid.NewGuid(), AccountHeaderNo = 500, AccountHeaderName = "Pengeluaran", AccountNo = 506, AccountName = "Beban Depresiasi" },
+            });
         }
     }
 }
