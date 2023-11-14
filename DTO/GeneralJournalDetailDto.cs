b58c203dbfba36f9ed630b589e0a88007b7819b0
AchLim <rimuakoru@gmail.com>
2023-11-15 01:52:29 +0700

    [ADD] GeneralJournalDetail DTO

---
+++ DTO/GeneralJournalDetailDto.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models.DTO
+{
+    public class GeneralJournalDetailDto
+    {
+        public Guid ChartOfAccountId { get; set; }
+
+        [Precision(19, 4)]
+        public decimal Debit { get; set; }
+
+        [Precision(19, 4)]
+        public decimal Credit { get; set; }
+    }
+    public class GeneralJournalDetailUpdateDto
+    {
+        public Guid? GeneralJournalHeaderId { get; set; }
+
+        public Guid? GeneralJournalDetailId { get; set; }
+
+        public Guid ChartOfAccountId { get; set; }
+
+        [Precision(19, 4)]
+        public decimal Debit { get; set; }
+
+        [Precision(19, 4)]
+        public decimal Credit { get; set; }
+    }
+}
