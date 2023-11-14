b7fb8b11a9946f4957eb8f18756773028de4a21a
AchLim <rimuakoru@gmail.com>
2023-11-15 01:52:22 +0700

    [ADD] GeneralJournalHeader DTO

---
+++ DTO/GeneralJournalHeaderDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models.DTO
+{
+    public class GeneralJournalHeaderDto
+    {
+        public string TransactionNo { get; set; } = default!;
+        public DateTime TransactionDate { get; set; }
+        public string? Description { get; set; }
+        public ICollection<GeneralJournalDetailDto>? GeneralJournalDetails { get; set; }
+    }
+    public class GeneralJournalHeaderUpdateDto
+    {
+        public Guid Id { get; set; }
+        public string TransactionNo { get; set; } = default!;
+        public DateTime TransactionDate { get; set; }
+        public string? Description { get; set; }
+        public ICollection<GeneralJournalDetailDto>? GeneralJournalDetails { get; set; }
+    }
+}
