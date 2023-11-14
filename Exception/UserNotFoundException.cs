53ae268d27e5081da639cd01005c1a66d383a8df
AchLim <rimuakoru@gmail.com>
2023-11-15 01:50:17 +0700

    [ADD] UserNotFoundException & Filter

---
+++ Exception/UserNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Exception
+{
+    public class UserNotFoundException : System.Exception
+    {
+        public UserNotFoundException(string? message, System.Exception? innerException = null) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Exception/UserNotFoundExceptionFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace WebAPI.Exception
+{
+    public class UserNotFoundExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            // Handle the exception and generate a response
+            if (context.Exception is UserNotFoundException ex)
+            {
+                var response = new { error = ex.Message };
+                var payload = JsonConvert.SerializeObject(response);
+                context.Result = new ContentResult
+                {
+                    Content = payload,
+                    ContentType = "application/json",
+                    StatusCode = (int)HttpStatusCode.Unauthorized,
+                };
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
