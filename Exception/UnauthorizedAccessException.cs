3a46c1ac6856963b5e5155e3374bbc84db53e43c
AchLim <rimuakoru@gmail.com>
2023-11-15 01:50:09 +0700

    [ADD] UnauthorizedAccessException & Filter

---
+++ Exception/UnauthorizedAccessException.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Exception
+{
+    public class UnauthorizedAccessException : System.Exception
+    {
+        public UnauthorizedAccessException(string? message, System.Exception? innerException = null) : base(message, innerException)
+        {
+        }
+    }
+}
