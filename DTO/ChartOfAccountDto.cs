6347bc50a00abcd6263c50120f141e6f0cba76a1
AchLim <rimuakoru@gmail.com>
2023-11-15 01:53:04 +0700

    [ADD] Chart of Account DTO

---
+++ DTO/ChartOfAccountDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models.DTO
+{
+    public class ChartOfAccountDto
+    {
+        public int AccountHeaderNo { get; set; }
+        public required string AccountHeaderName { get; set; }
+        public int AccountNo { get; set; }
+        public required string AccountName{ get; set; }
+    }
+    public class ChartOfAccountUpdateDto
+    {
+        public Guid Id { get; set; }
+        public int AccountHeaderNo { get; set; }
+        public required string AccountHeaderName { get; set; }
+        public int AccountNo { get; set; }
+        public required string AccountName { get; set; }
+    }
+}
