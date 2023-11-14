def9036638f9e71a3a8fd0a8b6afe85a0f68064e
AchLim <rimuakoru@gmail.com>
2023-11-15 01:51:23 +0700

    [ADD] GeneralJournalDetail Mapper

---
+++ Models/Mapper/GeneralJournalDetailMapper.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using WebAPI.Models.DTO;
+
+namespace WebAPI.Models.Mapper
+{
+    [Mapper]
+    public partial class GeneralJournalDetailMapper
+    {
+        public partial GeneralJournalDetailDto GeneralJournalDetailToGeneralJournalDetailDto(GeneralJournalDetail generalJournalDetail);
+        public partial GeneralJournalDetail GeneralJournalDetailDtoToGeneralJournalDetail(GeneralJournalDetailDto generalJournalDetailDto);
+        public partial GeneralJournalDetail GeneralJournalDetailUpdateDtoToGeneralJournalDetail(GeneralJournalDetailUpdateDto generalJournalDetailUpdateDto);
+    }
+}
