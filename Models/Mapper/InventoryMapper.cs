b6cc9814a4d1a6a985770eca3d842145e0f30f00
AchLim <rimuakoru@gmail.com>
2023-11-17 20:40:44 +0700

    [UPDATE] Add Inventory & Purchase Mapper

---
+++ Models/Mapper/InventoryMapper.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using WebAPI.Models.DTO;
+
+namespace WebAPI.Models.Mapper
+{
+    [Mapper]
+    public partial class InventoryMapper
+    {
+        public partial InventoryDto InventoryToInventoryDto(Inventory currency);
+        public partial Inventory InventoryDtoToInventory(InventoryDto currencyDto);
+        public partial Inventory InventoryUpdateDtoToInventory(InventoryUpdateDto currencyDto);
+    }
+}
---
+++ Models/Mapper/PurchaseMapper.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using WebAPI.Models.DTO;
+
+namespace WebAPI.Models.Mapper
+{
+    [Mapper]
+    public static partial class PurchaseHeaderMapper
+    {
+        public static partial PurchaseHeaderDto PurchaseHeaderToPurchaseHeaderDto(PurchaseHeader purchaseHeader);
+        public static partial PurchaseHeader PurchaseHeaderDtoToPurchaseHeader(PurchaseHeaderDto purchaseHeaderDto);
+        public static partial PurchaseHeader PurchaseHeaderUpdateDtoToPurchaseHeader(PurchaseHeaderUpdateDto purchaseHeaderUpdateDto);
+    }
+}
