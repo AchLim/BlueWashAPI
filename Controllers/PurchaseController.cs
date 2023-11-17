78e0b5e28527c9f5019e8d43421213b3b8be8632
AchLim <rimuakoru@gmail.com>
2023-11-17 20:41:52 +0700

    [UPDATE] Add CRUD for Purchase

--- Controllers/PurchaseController.cs
+++ Controllers/PurchaseController.cs
@@ -22,6 +22,60 @@ namespace WebAPI.Controllers
             _purchaseRepository = purchaseRepository;
         }
 
+        [HttpGet("header/all")]
+        public async Task<ActionResult<PurchaseHeader>> GetAllJournalEntriesAsync()
+        {
+            IEnumerable<PurchaseHeader> journalEntries = await _purchaseRepository.GetAllPurchaseHeaders();
+            return Ok(journalEntries);
+        }
+
+        [HttpGet("header/{id}")]
+        public async Task<ActionResult<PurchaseHeader>> GetPurchaseHeaderById(Guid id)
+        {
+            PurchaseHeader? purchaseHeader = await _purchaseRepository.GetPurchaseHeaderById(id);
+            return Ok(purchaseHeader);
+        }
+
+        [HttpPost("header/insert")]
+        public async Task<ActionResult<PurchaseHeader>> PostPurchaseHeader([FromBody] PurchaseHeaderDto purchaseHeaderDto)
+        {
+            PurchaseHeader purchaseHeader = PurchaseHeaderMapper.PurchaseHeaderDtoToPurchaseHeader(purchaseHeaderDto);
+            await _purchaseRepository.InsertPurchaseHeader(purchaseHeader);
+
+            return CreatedAtAction(nameof(GetPurchaseHeaderById), new { id = purchaseHeader.Id }, purchaseHeader);
+        }
+
+        [HttpPut("header/update/{id}")]
+        public async Task<ActionResult<PurchaseHeader>> UpdatePurchaseHeader(Guid id, [FromBody] PurchaseHeaderUpdateDto purchaseHeaderUpdateDto)
+        {
+            if (id != purchaseHeaderUpdateDto.Id)
+                return BadRequest("ID data pembelian tidak cocok!");
+
+            PurchaseHeader? purchaseHeader = await _purchaseRepository.GetPurchaseHeaderById(id);
+            if (purchaseHeader is null)
+                return BadRequest($"Data pembelian dengan id: {id} tidak ditemukan");
+
+            purchaseHeaderUpdateDto.PassData(ref purchaseHeader);
+            await _purchaseRepository.UpdatePurchaseHeader(purchaseHeader);
+
+            // Re-query to include all data.
+            purchaseHeader = await _purchaseRepository.GetPurchaseHeaderById(id);
+
+            return CreatedAtAction(nameof(GetPurchaseHeaderById), new { id = purchaseHeader!.Id }, purchaseHeader);
+        }
+
+        [HttpDelete("header/delete/{id}")]
+        public async Task<ActionResult> DeletePurchaseHeader(Guid id)
+        {
+            PurchaseHeader? purchaseHeader = await _purchaseRepository.GetPurchaseHeaderById(id);
+            if (purchaseHeader is null)
+                return BadRequest($"Data pembelian dengan id: {id} tidak ditemukan!");
+
+            await _purchaseRepository.DeletePurchaseHeader(purchaseHeader);
+
+            return Ok();
+        }
+        
         [HttpGet("get_total_item_purchase")]
         [AllowAnonymous]
         public async Task<ActionResult<ItemPurchaseContainer>> GetTotalItemPurchase()
